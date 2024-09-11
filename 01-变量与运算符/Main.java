d812cfa99a491b3cd6f73913927e515be5fe9d36
chenjie <chenjie@chenjiedeMacBook-Pro.local>
2024-09-11 16:30:39 +0800

    feat: 变量与运算符

---
+++ 01-变量与运算符/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // 计算并输出圆的面积
+        double pi = 3.14;
+
+        double r1 = 1.2;
+        double r2 = 1.5;
+        int r3 = 6;
+
+        double s1 = pi * r1 * r1;
+        double s2 = pi * r2 * r2;
+        double s3 = pi * r3 * r3;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
