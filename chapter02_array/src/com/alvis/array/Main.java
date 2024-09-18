b6456766d8833748988f5a431207c7cbf1317628
陈杰 <18822126754@163.com>
2024-09-18 21:14:20 +0800

    feat: 数组的初始化

---
+++ chapter02_array/src/com/alvis/array/Main.java
@@ -0,0 +1,27 @@
+package com.alvis.array;
+
+/**
+ * ClassName: Main
+ * Package: com.alvis.array
+ * Description: 数组
+ *
+ * @Author: chenjie
+ * @Create: 2024/9/18 19:45
+ */
+public class Main {
+
+    public static void main(String[] args) {
+         // 1. 数组初始化
+        double[] myList;
+        myList = new double[]{12.0, 0.12, 12.3, 17.66};
+
+        String[] foods;
+        // foods = new String[]{"豆腐", "西红柿", "鸡蛋"};
+        foods = new String[3];
+
+        // 2. 数组元素的调用
+        
+
+    }
+
+}
